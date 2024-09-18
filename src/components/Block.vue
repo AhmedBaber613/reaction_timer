0dda93bfa8abcf6d21715accca19b63a463ac171
Ahmed Baber <145803188+AhmedBaber613@users.noreply.github.com>
2024-09-18 12:25:58 +0500

    project done

---
+++ src/components/Block.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    click me
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['delay'],
+
+    data() {
+      return {
+        showBlock: false,
+        timer: null,
+        reactionTime: 0
+      }
+    },
+
+    mounted() {
+      setTimeout(() => {
+        this.showBlock = true
+        this.startTimer()
+      }, this.delay)
+    },
+
+    methods: {
+      startTimer() {
+        this.timer = setInterval(() => {
+          this.reactionTime += 10
+        }, 10)
+      },
+
+      stopTimer() {
+        clearInterval(this.timer)
+        this.$emit('end', this.reactionTime)
+        console.log(this.reactionTime)
+      }
+    }
+  }
+</script>
+
+<style>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
+
+//20604090A
+//https://us05web.zoom.us/j/4544299851?pwd=fOA6rZ2ab688TyqHn9uzt3c7rLXfV0.1&omn=89701135834
